1f9a3988a5363bcbc8246538bbfa69f20ab033f4
Neil Kalman <Neilkalman@gmail.com>
2021-01-13 20:18:38 +0200

    feat(console): add console.log when app is loaded in browser

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { consoleLogo } from '@kibibit/consologo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  title = 'kibibit client';
+
+  constructor() {
+    consoleLogo('kibibit client', 'noice!');
+  }
 }
