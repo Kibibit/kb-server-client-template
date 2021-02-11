9cd8bb654b80c85c36c49d06b1627a37fd23bfc7
Neil Kalman <Neilkalman@gmail.com>
2021-02-11 23:03:33 +0200

    fix(tests): fix client tests and test outside devcontainer
    made sure coverage is reported for client side, and that both client and server output to the same
    root coverage folder

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -9,18 +9,33 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-spec-reporter')
     ],
     client: {
+      captureConsole: false,
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/client'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+    preprocessors: {
+      'src/**/*!(test|spec).ts': ['coverage']
+    },
+    // coverageIstanbulReporter: {
+    //   dir: require('path').join(__dirname, '../coverage/client'),
+    //   reports: ['html', 'lcovonly', 'text-summary'],
+    //   fixWebpackSourcePaths: true
+    // },
+    coverageReporter: {
+      // type : 'html',
+      dir : '../coverage/client',
+      reporters: [
+        { type: 'lcov', subdir: 'report-html' },
+        { type: 'html', subdir: 'report-lcov' }
+      ],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    
+    reporters: ['spec', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import {
+  AngularMaterialModule
+} from './angular-material/angular-material.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AngularMaterialModule
       ],
       declarations: [
         AppComponent
@@ -21,17 +25,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'kibibit client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('kibibit client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent)
-      .toContain('client app is running!');
+    expect(compiled.querySelector('#site-name').textContent)
+      .toContain('kibibit Client Side');
   });
 });
