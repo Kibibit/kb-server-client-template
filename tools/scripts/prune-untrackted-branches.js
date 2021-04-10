fed358d4305a7315f71cdb30587db8207b6a79de
thatkookooguy <neilkalman@gmail.com>
2021-04-10 23:16:00 +0000

    feat(tools): add prune-branches as a npm script + catch errors

--- tools/scripts/prune-untrackted-branches.js
+++ tools/scripts/prune-untrackted-branches.js
@@ -13,74 +13,78 @@ const chars = {
 };
 
 (async () => {
-  const git = simpleGit();
-  await git.fetch(['-p']); // prune? is it necassery?
-  const branchSummaryResult = await git.branch(['-vv']);
-  const localBranches = branchSummaryResult.branches;
-  const localBranchesWithGoneRemotes = chain(localBranches)
-    .filter((item) => !['master', 'next'].includes(item.name))
-    .forEach((item) => {
-      // console.log('an item appeared!', item);
-      const remoteInfo = item.label.match(remoteInfoRegex);
+  try {
+    const git = simpleGit();
+    await git.fetch(['-p']); // prune? is it necassery?
+    const branchSummaryResult = await git.branch(['-vv']);
+    const localBranches = branchSummaryResult.branches;
+    const localBranchesWithGoneRemotes = chain(localBranches)
+      .filter((item) => !['master', 'next'].includes(item.name))
+      .forEach((item) => {
+        // console.log('an item appeared!', item);
+        const remoteInfo = item.label.match(remoteInfoRegex);
 
-      if (remoteInfo) {
-        const parsedRemoteInfo = trim(remoteInfo[0], '[] ');
-        const isMerged = parsedRemoteInfo.endsWith(': gone');
-        const remoteBranchName = parsedRemoteInfo.replace(': gone', '');
+        if (remoteInfo) {
+          const parsedRemoteInfo = trim(remoteInfo[0], '[] ');
+          const isMerged = parsedRemoteInfo.endsWith(': gone');
+          const remoteBranchName = parsedRemoteInfo.replace(': gone', '');
 
-        item.isMerged = isMerged;
-        item.remoteName = remoteBranchName;
-      }
-    })
-    .filter((item) => item.isMerged)
-    .value();
+          item.isMerged = isMerged;
+          item.remoteName = remoteBranchName;
+        }
+      })
+      .filter((item) => item.isMerged)
+      .value();
     const branchNames = chain(localBranchesWithGoneRemotes).map('name').value();
 
-  // interaction!
-  const table = new Table({
-    head: ['Branch Name', 'Origin Branch Name', 'Origin Branch Status'],
-    chars
-  });
+    // interaction!
+    const table = new Table({
+      head: ['Branch Name', 'Origin Branch Name', 'Origin Branch Status'],
+      chars
+    });
 
-  localBranchesWithGoneRemotes.forEach((item) => table.push([item.name, item.remoteName, 'GONE']));
+    localBranchesWithGoneRemotes.forEach((item) => table.push([item.name, item.remoteName, 'GONE']));
 
-  console.log(table.toString());
+    console.log(`${table.toString()}\n`);
 
-  const ACTION_ANSWERS = {
-    PRUNE_ALL: 'prune all gone branches',
-    SELECT_BRANCHES: 'Selected Individual branches to delete'
-  }
+    const ACTION_ANSWERS = {
+      PRUNE_ALL: 'prune all gone branches',
+      SELECT_BRANCHES: 'Selected Individual branches to delete'
+    }
 
-  const answers = await inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'whatToDo',
-        message: 'These branches have been deleted from the origin. What do you want to do with them?',
-        choices: chain(ACTION_ANSWERS).values().value()
-      }
-    ]);
-
-  if (answers.whatToDo === ACTION_ANSWERS.PRUNE_ALL) { 
-    console.log(branchNames);
-    await git.deleteLocalBranches(branchNames);
-    console.log('DONE');
-    return;
-  }
-
-  if (answers.whatToDo === ACTION_ANSWERS.SELECT_BRANCHES) {
     const answers = await inquirer
       .prompt([
         {
-          type: 'checkbox',
-          message: 'These branches have been deleted from the origin. Do you want to prune them?',
-          name: 'pruneBranches',
-          choices: branchNames
+          type: 'list',
+          name: 'whatToDo',
+          message: 'These branches have been deleted from the origin. What do you want to do with them?',
+          choices: chain(ACTION_ANSWERS).values().value()
         }
       ]);
 
-    await Promise.all(answers.pruneBranches.map((branchName) => git.deleteLocalBranch(branchName)));
-    console.log('DONE');
-    return;
+    if (answers.whatToDo === ACTION_ANSWERS.PRUNE_ALL) {
+      await git.deleteLocalBranches(branchNames);
+      console.log('DONE');
+      return;
+    }
+
+    if (answers.whatToDo === ACTION_ANSWERS.SELECT_BRANCHES) {
+      const answers = await inquirer
+        .prompt([
+          {
+            type: 'checkbox',
+            message: 'These branches have been deleted from the origin. Do you want to prune them?',
+            name: 'pruneBranches',
+            choices: branchNames
+          }
+        ]);
+
+      await Promise.all(answers.pruneBranches.map((branchName) => git.deleteLocalBranch(branchName)));
+      console.log('DONE');
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
 })();
